69b3ff6230057db7a55c9e7c00335a76b303ecd0
Moritz Wright <me@moritzwright.com>
2022-02-25 14:30:04 -0600

    yml check

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
-import Image from "next/image";
-import Link from "next/link";
-import ReactPlayer from "react-player";
-import useEmblaCarousel from "embla-carousel-react";
-import Autoplay from "embla-carousel-autoplay";
-import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
+// import Image from "next/image";
+// import Link from "next/link";
+// import ReactPlayer from "react-player";
+// import useEmblaCarousel from "embla-carousel-react";
+// import Autoplay from "embla-carousel-autoplay";
+// import { useRouter } from "next/router";
+// import { useState, useEffect } from "react";
 import Script from "next/script";
 
 // styles
 import styles from "../styles/Main.module.css";
 
 // components
-import NavBar from "../components/NavBar";
-import Footer from "../components/Footer";
-import TextBlock from "../components/TextBlock";
-import InfoCard from "../components/InfoCard";
-import Protagonist from "../components/Protagonist";
+// import NavBar from "../components/NavBar";
+// import Footer from "../components/Footer";
+// import TextBlock from "../components/TextBlock";
+// import InfoCard from "../components/InfoCard";
+// import Protagonist from "../components/Protagonist";
 
 export default function Home({ content }) {
   console.log(content, "<-----");
