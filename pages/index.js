94c3fff4983256fed44f14e12dd3c3a08ee03d94
Moritz Wright <me@moritzwright.com>
2022-02-25 14:47:11 -0600

    random comp

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,9 @@ import styles from "../styles/Main.module.css";
 // components
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
-import TextBlock from "../components/TextBlock";
+// import TextBlock from "../components/TextBlock";
 // import InfoCard from "../components/InfoCard";
-// import Protagonist from "../components/Protagonist";
+import Protagonist from "../components/Protagonist";
 
 export default function Home({ content }) {
   console.log(content, "<-----");
